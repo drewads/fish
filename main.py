2d88a32cf19e16ad0b54bcee8931971fe76596c4
Theo Culhane <theo@teamculhane.com>
2023-03-02 15:59:06 -0800

    Fixed argparse lol

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-                                     prog='Literature (Fish) Player',
                                      description='CS 238 Final Project for Drew, Theo, and Tristan that plays the game Literature (https://en.wikipedia.org/wiki/Literature_(card_game))')
     parser.add_argument('--model', type=str, required=True, choices=['simple_rules'])
     args = parser.parse_args()
