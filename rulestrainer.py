cddd14081b1fdeff3951b32b61aaba4fe69cec8c
tsaucedo <tristansaucedo1999@gmail.com>
2023-03-02 14:14:00 -0800

    initialize a current game

--- rulestrainer.py
+++ rulestrainer.py
@@ -1,2 +1,9 @@
 # this will be a class or function that instantiates a Fish game and some models
 # and then communicates between game and models
+
+# initialize a trainer
+import fish
+
+current_game = fish()
+
+
