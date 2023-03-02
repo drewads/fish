d6920e984d708c1aa16a047d3df5df8fad26808c
tsaucedo <tristansaucedo1999@gmail.com>
2023-03-02 15:59:15 -0800

    trainer

--- rulestrainer.py
+++ rulestrainer.py
@@ -3,8 +3,8 @@ import rulesmodel
 import sys
 import random
 
-PLAYERS = 6 # number of players
-TEAM_LEN = 3 # number of players per team
+from fish import PLAYERS, TEAM_LEN, HS_LEN, DECK_LEN
+
 
 # this will be a class or function that instantiates a Fish game and some models
 # and then communicates between game and models
@@ -44,17 +44,38 @@ def get_team(player):
 
 def play_game():
     turns = 0
-    sets_left = 9
+    turns_per_player = [0 for i in range(PLAYERS)]
+    sets_left = HS_LEN/DECK_LEN
     players = [i for i in range(PLAYERS)]
 
     current_game = fish.Fish()
-    print(current_game.cards)
-
-
-    start_cards = []
-    models = [rulesmodel(i, get_team(i), get_otherteam(i), start_cards[i]) for i in players]
+    start_cards = [list(current_game.cards[i]) for i in players]
+    
+    print(start_cards)
+    models = [rulesmodel.RulesModel(i, get_team(i), get_otherteam(i), start_cards[i]) for i in players]
+    current_player = 0
     while (sets_left > 0):
-        pass
+
+        # have the model(player) decide what card to ask for
+        (action, action_support) = models[current_player].take_action()
+
+        if action == -1: # declared a halfsuit
+            pass
+        else:
+            askee = action_support[0]
+            card = action_support[1]
+            transfer = current_game.perform_action(current_player, askee, card)
+            
+            # update who has what card in the fish game
+            for player in players: # each player records the action
+                models[player].record_action(current_player, askee, card, transfer)
+            
+            if transfer:
+                current_player = askee
+        
+        # check if a player declared a halfsuit, and then decriment it
+        turns += 1
+        turns_per_player[current_player] += 1
 
     return
 
